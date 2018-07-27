bff6cbaddc4c7d7d90f12bcaab8f3bddbf1b4441
Chelsea Beckton <cpb15@my.fsu.edu>
2018-07-27 18:20:30 +0000

    Lesson 7 final commit

--- drill4.js
+++ drill4.js
@@ -15,7 +15,9 @@ function display(event) {
 //attach event listener to h3 elements to invoke display function when clicked
 $("h3").click(display);
 
-//displays and animates the next element after the current target function display2(event) {
+//displays and animates the next element after the current target 
+function display2(event) {
 $(event.currentTarget).next().animate( {height: ‘toggle’}, “slow”); }//end of display2
-//attach event listener to h3 elements to invoke display function when clicked $("h3").click(display2);
-//$("h3").click(display);
+
+//attach event listener to h3 elements to invoke display function when clicked 
+$("h3").click(display2);
